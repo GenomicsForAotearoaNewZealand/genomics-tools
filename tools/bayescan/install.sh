e02b569d1f46ea147c94b7524b3cfdda4e024eae
Rob Elshire <rob@elshiregroup.co.nz>
2019-08-14 16:15:03 +1200

    updated license, removed code to add to user path, removed code related to user debian.

--- tools/bayescan/install.sh
+++ tools/bayescan/install.sh
@@ -11,26 +11,16 @@ apt-get -y install unzip wget
 
 mkdir /tmp/install
 
-chown debian:debian /tmp/install
-
 cd /tmp/install
 
 wget http://cmpg.unibe.ch/software/BayeScan/files/BayeScan2.1.zip
 
 unzip BayeScan2.1.zip
 
-
 # make the executable directory in opt, move binary there
 
 mkdir /opt/bayescan2.1
 
-chown debian:debian /opt/bayescan2.1
-
 mv /tmp/install/BayeScan2.1/binaries/BayeScan2.1_linux64bits /opt/bayescan2.1/bayescan
 
 chmod +x /opt/bayescan2.1/bayescan
-
-# put bayescan into path.
-
-echo '# The following puts the bayescan executable in the user path.' >> ~/.bashrc
-echo 'export PATH="/opt/bayescan2.1:$PATH"' >> ~/.bashrc
