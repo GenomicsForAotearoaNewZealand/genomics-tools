6c1cfd4df49279b75119205bc95926e7b36013fd
Rob Elshire <rob@elshiregroup.co.nz>
2019-08-14 14:44:43 +1200

    added the tool bwa using debian 10 as the base docker image. This is because the newer version is preferred and to test whether changing the docker file to debian 10 works as expected.

---
+++ tools/bwa/install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# This program is to set up a docker image for the bwa.
+# It assumes a Debian 10 base image has been used for the container.
+
+# install bwa
+apt-get -y install  bwa
---
+++ tools/bwa/test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Try which bwa and see if it is installed.
+# If it does, then put GFANZ_TEST_RESULTS=OK into syslog. If it does not, then 
+# put GFANZ_TEST_RESULTS=ERROR into syslog.
+which bwa | grep /usr/bin/bwa >> /dev/null
+
+if [ $? -eq 0 ]; then
+  echo "GFANZ_TEST_RESULTS=OK"
+else
+  echo "GFANZ_TEST_RESULTS=ERROR"
+fi
