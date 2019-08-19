15e169df12a7631ce13fb047873c177bde32a57b
Rob Elshire <rob@elshiregroup.co.nz>
2019-08-19 17:55:23 +1200

    removed the line in test.sh referring to hello world.

--- tools/gbs-snp-crop/test.sh
+++ tools/gbs-snp-crop/test.sh
@@ -3,7 +3,6 @@
 # Try GBS-SNP-CROP-1.pl and see if it returns GBS-SNP-CROP
 # If it does, then output GFANZ_TEST_RESULTS=OK. If it does not, then output
 # GFANZ_TEST_RESULTS=ERROR.
-/opt/helloworld.sh | grep Hello >> /dev/null
 
 /opt/GBS-SNP-CROP/GBS-SNP-CROP-scripts/v.4.0/GBS-SNP-CROP-1.pl  2>&1 | grep GBS-SNP-CROP >> /dev/null
 
