bce6324e2b80cf294bef5a5e38351b6e40260a9a
Rob Elshire <rob@elshiregroup.co.nz>
2019-09-03 11:15:23 +1200

    Added fastp to install script.

--- tools/ddocent/install.sh
+++ tools/ddocent/install.sh
@@ -3,7 +3,7 @@
 # This program is to set up a docker container for the tool dDocent version 2.7.8.
 # It depends on a standard Debian 9 docker image.
 
-apt-get install -y locales wget freebayes stacks trimmomatic mawk bwa samtools vcftools bio-rainbow seqtk cd-hit bedtools libvcflib1 libvcflib-tools gnuplot parallel bamtools openjdk-11-jre-headless git
+apt-get install -y locales wget freebayes stacks trimmomatic mawk bwa samtools vcftools bio-rainbow seqtk cd-hit bedtools libvcflib1 libvcflib-tools gnuplot parallel bamtools openjdk-11-jre-headless fastp git
 
 # dDocent calls rainbow, but debian calls that programme bio-rainbow. 
 # make a soft link from /usr/bin/rainbow /usr/bin/bio-rainbow
