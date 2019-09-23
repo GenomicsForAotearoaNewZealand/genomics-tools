9513ca4d7edd2e236b3b88bdbfebc0b6d1d766fb
Shannon Ormond <sormond@Shannons-MBP.fritz.box>
2019-09-23 13:27:45 +1200

    added wget install command

--- tools/mosdepth/install.sh
+++ tools/mosdepth/install.sh
@@ -3,5 +3,7 @@
 # This program is to set up a docker image for mosdepth 0.2.6.
 # It assumes a Debian 9 base image has been used for the container.
 
+apt-get install -y wget
+
 # install mosdepth
 wget https://github.com/brentp/mosdepth/releases/download/v0.2.6/mosdepth && chmod +x ./mosdepth
