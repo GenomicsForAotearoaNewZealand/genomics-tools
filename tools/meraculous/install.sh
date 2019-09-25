3c688b0329d1b4a28df0e9c3711bccf8cb190cb3
Gillian Gibb <gillian.c.gibb@gmail.com>
2019-09-25 13:32:20 +1200

    update install file

--- tools/meraculous/install.sh
+++ tools/meraculous/install.sh
@@ -14,4 +14,5 @@ wget https://sourceforge.net/projects/meraculous20/files/latest/download
 mv download Meraculous-v2.2.6.tar.gz
 tar -xzf Meraculous-v2.2.6.tar.gz
 apt-get install -y build-essential cmake libboost-all-dev liblog-log4perl-perl gnuplot
+cd Meraculous-v2.2.6
 ./install.sh /opt/meraculous
