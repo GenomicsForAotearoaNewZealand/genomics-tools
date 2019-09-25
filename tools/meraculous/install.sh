1881eb679c6855928d3cefafe0581bb9b49d5c05
Gibb <gcgibb@it079846.massey.ac.nz>
2019-09-25 13:02:06 +1200

    Inital commit for Meraculous-2D v2.2.6

---
+++ tools/meraculous/install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# This program is to set up a docker image for Meraculous-2D.
+# It assumes a Debian 9 base image has been used for the container.
+
+aptget -y install wget
+
+# download and install Meraculous
+
+cd /tmp
+mkdir meraculous
+cd meraculous/
+wget https://sourceforge.net/projects/meraculous20/files/latest/download
+mv download Meraculous-v2.2.6.tar.gz
+tar -xzf Meraculous-v2.2.6.tar.gz
+apt-get install -y build-essential cmake libboost-all-dev liblog-log4perl-perl gnuplot
+./install.sh /opt/meraculous
