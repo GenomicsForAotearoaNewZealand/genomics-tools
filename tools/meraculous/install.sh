c27f8c4515f540246b8e55e055ddfcaca6e0fbba
Tim McNamara <code@timmcnamara.co.nz>
2019-10-23 21:02:28 +0000

    Parameterise version number
    Although the package is marked as "Abandoned" on
    SourceForge, it's possible that an update will be
    released anyway. That would trigger the "latest"
    file to change, which would in turn break the script.
    
    As a drive-by, refactored a few parts of the script:
     - abort early if an intermediate step fails
     - use mkdir -p for increased readability
     - remove the working directory once the install is complete
     - install all dependencies in a single place
     - move each dependency on its own line, simplifying the
       diff of any future required changes

--- tools/meraculous/install.sh
+++ tools/meraculous/install.sh
@@ -3,16 +3,31 @@
 # This program is to set up a docker image for Meraculous-2D.
 # It assumes a Debian 9 base image has been used for the container.
 
-apt-get -y install wget
+set -e # exit early if one the commands fails
+set -x # echo commands as they're run so that debugging is easier
 
-# download and install Meraculous
+VERSION=2.2.6
 
-cd /tmp
-mkdir meraculous
-cd meraculous/
-wget https://sourceforge.net/projects/meraculous20/files/latest/download
-mv download Meraculous-v2.2.6.tar.gz
-tar -xzf Meraculous-v2.2.6.tar.gz
-apt-get install -y build-essential cmake libboost-all-dev liblog-log4perl-perl gnuplot
-cd Meraculous-v2.2.6
+# install dependencies
+apt-get -y update
+apt-get -y install \
+                build-essential \
+                cmake \
+                libboost-all-dev \
+                liblog-log4perl-perl \
+                gnuplot \
+                wget
+
+# download
+mkdir -p /tmp/meraculous
+cd /tmp/meraculous
+wget https://sourceforge.net/projects/meraculous20/files/Meraculous-v"$VERSION".tar.gz/download
+mv download Meraculous-v$VERSION.tar.gz
+tar -xzf Meraculous-v$VERSION.tar.gz
+cd Meraculous-v$VERSION
+
+# install
 ./install.sh /opt/meraculous
+
+# clean up
+rm -rf /tmp/meraculous
